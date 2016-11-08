84297ff0939d2352f7be07fc8b01e265785c0b26
StephanRaab <sarcartist@gmail.com>
2016-11-07 19:08:12 -0500

    api dispaying data from search

--- movie_search_app/app.js
+++ movie_search_app/app.js
@@ -2,12 +2,13 @@ var express = require('express');
 var app = express();
 
 var request = require('request');
+app.set("view engine", "ejs");
 
 app.get("/results", function(req, res){
   request("http://omdbapi.com/?s=karate", function(error, response, body){
     if(!error && response.statusCode == 200){
-      var parsedBody = JSON.parse(body);
-      res.send(parsedBody["Search"][0]["Title"]);
+      var data = JSON.parse(body);
+      res.render("results", {data: data});
     }
   });
 });
